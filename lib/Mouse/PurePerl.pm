0741a2874a45664f8fb9a8ef92b2f66993970a35
藤 吾郎 <gfuji@cpan.org>
2013-01-11 09:54:59 +0000

    Import of GFUJI/Mouse-1.04 from CPAN.
    gitpan-cpan-distribution: Mouse
    gitpan-cpan-version:      1.04
    gitpan-cpan-path:         GFUJI/Mouse-1.04.tar.gz
    gitpan-cpan-author:       GFUJI
    gitpan-cpan-maturity:     released

--- lib/Mouse/PurePerl.pm
+++ lib/Mouse/PurePerl.pm
@@ -437,7 +437,12 @@ sub handles              { $_[0]->{handles}                }
 
 sub _is_metadata         { $_[0]->{is}                     }
 sub is_required          { $_[0]->{required}               }
-sub default              { $_[0]->{default}                }
+sub default {
+    my($self, $instance) = @_;
+    my $value = $self->{default};
+    $value = $value->($instance) if defined($instance) and ref($value) eq "CODE";
+    return $value;
+}
 sub is_lazy              { $_[0]->{lazy}                   }
 sub is_lazy_build        { $_[0]->{lazy_build}             }
 sub is_weak_ref          { $_[0]->{weak_ref}               }
@@ -750,7 +755,7 @@ Mouse::PurePerl - A Mouse guts in pure Perl
 
 =head1 VERSION
 
-This document describes Mouse version 1.03
+This document describes Mouse version 1.04
 
 =head1 SEE ALSO
 
