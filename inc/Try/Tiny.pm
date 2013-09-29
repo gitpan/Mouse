ddf6d588ec71c60f1fb6be90d8aa6ca087fd4954
藤 吾郎 <gfuji@cpan.org>
2013-09-29 00:56:23 +0000

    Import of GFUJI/Mouse-1.12 from CPAN.
    gitpan-cpan-distribution: Mouse
    gitpan-cpan-version:      1.12
    gitpan-cpan-path:         GFUJI/Mouse-1.12.tar.gz
    gitpan-cpan-author:       GFUJI
    gitpan-cpan-maturity:     released

--- inc/Try/Tiny.pm
+++ inc/Try/Tiny.pm
@@ -1,138 +1,150 @@
 #line 1
 package Try::Tiny;
+BEGIN {
+  $Try::Tiny::AUTHORITY = 'cpan:NUFFIN';
+}
+{
+  $Try::Tiny::VERSION = '0.18';
+}
+use 5.006;
+# ABSTRACT: minimal try/catch with proper preservation of $@
 
 use strict;
-#use warnings;
+use warnings;
 
-use vars qw(@EXPORT @EXPORT_OK $VERSION @ISA);
+use base 'Exporter';
+our @EXPORT = our @EXPORT_OK = qw(try catch finally);
 
-BEGIN {
-	require Exporter;
-	@ISA = qw(Exporter);
-}
-
-$VERSION = "0.12";
-
-$VERSION = eval $VERSION;
-
-@EXPORT = @EXPORT_OK = qw(try catch finally);
-
+use Carp;
 $Carp::Internal{+__PACKAGE__}++;
+
+BEGIN { eval "use Sub::Name; 1" or *{subname} = sub {1} }
 
 # Need to prototype as @ not $$ because of the way Perl evaluates the prototype.
 # Keeping it at $$ means you only ever get 1 sub because we need to eval in a list
 # context & not a scalar one
 
 sub try (&;@) {
-	my ( $try, @code_refs ) = @_;
+  my ( $try, @code_refs ) = @_;
 
-	# we need to save this here, the eval block will be in scalar context due
-	# to $failed
-	my $wantarray = wantarray;
+  # we need to save this here, the eval block will be in scalar context due
+  # to $failed
+  my $wantarray = wantarray;
 
-	my ( $catch, @finally );
+  # work around perl bug by explicitly initializing these, due to the likelyhood
+  # this will be used in global destruction (perl rt#119311)
+  my ( $catch, @finally ) = ();
 
-	# find labeled blocks in the argument list.
-	# catch and finally tag the blocks by blessing a scalar reference to them.
-	foreach my $code_ref (@code_refs) {
-		next unless $code_ref;
+  # find labeled blocks in the argument list.
+  # catch and finally tag the blocks by blessing a scalar reference to them.
+  foreach my $code_ref (@code_refs) {
 
-		my $ref = ref($code_ref);
+    if ( ref($code_ref) eq 'Try::Tiny::Catch' ) {
+      croak 'A try() may not be followed by multiple catch() blocks'
+        if $catch;
+      $catch = ${$code_ref};
+    } elsif ( ref($code_ref) eq 'Try::Tiny::Finally' ) {
+      push @finally, ${$code_ref};
+    } else {
+      croak(
+        'try() encountered an unexpected argument ('
+      . ( defined $code_ref ? $code_ref : 'undef' )
+      . ') - perhaps a missing semi-colon before or'
+      );
+    }
+  }
 
-		if ( $ref eq 'Try::Tiny::Catch' ) {
-			$catch = ${$code_ref};
-		} elsif ( $ref eq 'Try::Tiny::Finally' ) {
-			push @finally, ${$code_ref};
-		} else {
-			use Carp;
-			confess("Unknown code ref type given '${ref}'. Check your usage & try again");
-		}
-	}
+  # FIXME consider using local $SIG{__DIE__} to accumulate all errors. It's
+  # not perfect, but we could provide a list of additional errors for
+  # $catch->();
 
-	# save the value of $@ so we can set $@ back to it in the beginning of the eval
-	my $prev_error = $@;
+  # name the blocks if we have Sub::Name installed
+  my $caller = caller;
+  subname("${caller}::try {...} " => $try);
+  subname("${caller}::catch {...} " => $catch) if $catch;
+  subname("${caller}::finally {...} " => $_) foreach @finally;
 
-	my ( @ret, $error, $failed );
+  # save the value of $@ so we can set $@ back to it in the beginning of the eval
+  # and restore $@ after the eval finishes
+  my $prev_error = $@;
 
-	# FIXME consider using local $SIG{__DIE__} to accumulate all errors. It's
-	# not perfect, but we could provide a list of additional errors for
-	# $catch->();
+  my ( @ret, $error );
 
-	{
-		# localize $@ to prevent clobbering of previous value by a successful
-		# eval.
-		local $@;
+  # failed will be true if the eval dies, because 1 will not be returned
+  # from the eval body
+  my $failed = not eval {
+    $@ = $prev_error;
 
-		# failed will be true if the eval dies, because 1 will not be returned
-		# from the eval body
-		$failed = not eval {
-			$@ = $prev_error;
+    # evaluate the try block in the correct context
+    if ( $wantarray ) {
+      @ret = $try->();
+    } elsif ( defined $wantarray ) {
+      $ret[0] = $try->();
+    } else {
+      $try->();
+    };
 
-			# evaluate the try block in the correct context
-			if ( $wantarray ) {
-				@ret = $try->();
-			} elsif ( defined $wantarray ) {
-				$ret[0] = $try->();
-			} else {
-				$try->();
-			};
+    return 1; # properly set $fail to false
+  };
 
-			return 1; # properly set $fail to false
-		};
+  # preserve the current error and reset the original value of $@
+  $error = $@;
+  $@ = $prev_error;
 
-		# copy $@ to $error; when we leave this scope, local $@ will revert $@
-		# back to its previous value
-		$error = $@;
-	}
-
-	# set up a scope guard to invoke the finally block at the end
-	my @guards =
+  # set up a scope guard to invoke the finally block at the end
+  my @guards =
     map { Try::Tiny::ScopeGuard->_new($_, $failed ? $error : ()) }
     @finally;
 
-	# at this point $failed contains a true value if the eval died, even if some
-	# destructor overwrote $@ as the eval was unwinding.
-	if ( $failed ) {
-		# if we got an error, invoke the catch block.
-		if ( $catch ) {
-			# This works like given($error), but is backwards compatible and
-			# sets $_ in the dynamic scope for the body of C<$catch>
-			for ($error) {
-				return $catch->($error);
-			}
+  # at this point $failed contains a true value if the eval died, even if some
+  # destructor overwrote $@ as the eval was unwinding.
+  if ( $failed ) {
+    # if we got an error, invoke the catch block.
+    if ( $catch ) {
+      # This works like given($error), but is backwards compatible and
+      # sets $_ in the dynamic scope for the body of C<$catch>
+      for ($error) {
+        return $catch->($error);
+      }
 
-			# in case when() was used without an explicit return, the C<for>
-			# loop will be aborted and there's no useful return value
-		}
+      # in case when() was used without an explicit return, the C<for>
+      # loop will be aborted and there's no useful return value
+    }
 
-		return;
-	} else {
-		# no failure, $@ is back to what it was, everything is fine
-		return $wantarray ? @ret : $ret[0];
-	}
+    return;
+  } else {
+    # no failure, $@ is back to what it was, everything is fine
+    return $wantarray ? @ret : $ret[0];
+  }
 }
 
 sub catch (&;@) {
-	my ( $block, @rest ) = @_;
+  my ( $block, @rest ) = @_;
 
-	return (
-		bless(\$block, 'Try::Tiny::Catch'),
-		@rest,
-	);
+  croak 'Useless bare catch()' unless wantarray;
+
+  return (
+    bless(\$block, 'Try::Tiny::Catch'),
+    @rest,
+  );
 }
 
 sub finally (&;@) {
-	my ( $block, @rest ) = @_;
+  my ( $block, @rest ) = @_;
 
-	return (
-		bless(\$block, 'Try::Tiny::Finally'),
-		@rest,
-	);
+  croak 'Useless bare finally()' unless wantarray;
+
+  return (
+    bless(\$block, 'Try::Tiny::Finally'),
+    @rest,
+  );
 }
 
 {
   package # hide from PAUSE
     Try::Tiny::ScopeGuard;
+
+  use constant UNSTABLE_DOLLARAT => ($] < '5.013002') ? 1 : 0;
 
   sub _new {
     shift;
@@ -140,9 +152,22 @@ sub finally (&;@) {
   }
 
   sub DESTROY {
-    my @guts = @{ shift() };
-    my $code = shift @guts;
-    $code->(@guts);
+    my ($code, @args) = @{ $_[0] };
+
+    local $@ if UNSTABLE_DOLLARAT;
+    eval {
+      $code->(@args);
+      1;
+    } or do {
+      warn
+        "Execution of finally() block $code resulted in an exception, which "
+      . '*CAN NOT BE PROPAGATED* due to fundamental limitations of Perl. '
+      . 'Your program will continue as if this event never took place. '
+      . "Original exception text follows:\n\n"
+      . (defined $@ ? $@ : '$@ left undefined...')
+      . "\n"
+      ;
+    }
   }
 }
 
@@ -150,5 +175,4 @@ __PACKAGE__
 
 __END__
 
-#line 603
-
+#line 656
